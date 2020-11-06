c44c6fcf2112dedd85688b85f57e9f1ec234d51b
Neibye <60384297+Neibye@users.noreply.github.com>
2020-11-06 13:22:49 +0100

    firebase setup

---
+++ main.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyAaIaeGjKm9Qzl0P5HMFDQ3cP06ti2-m9M",
+    authDomain: "arlab2b-43131.firebaseapp.com",
+    databaseURL: "https://arlab2b-43131.firebaseio.com",
+    projectId: "arlab2b-43131",
+    storageBucket: "arlab2b-43131.appspot.com",
+    messagingSenderId: "528388439338",
+    appId: "1:528388439338:web:cd49acdb925f701612b253"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
