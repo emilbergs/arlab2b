c415e9c594206d3feb1bea7cd01f1c75320fd0ba
Neibye <60384297+Neibye@users.noreply.github.com>
2020-11-07 18:55:33 +0100

    data side

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
   const db = firebase.firestore();
+  const yearOne = db.collection("yearOne")
   const yearSix = db.collection("yearSix");
 
 
@@ -184,3 +185,106 @@ function skiftRegion() {
   }
   document.querySelector("#gårdIRegion").innerHTML = template
 }
+
+
+/* charts */
+
+
+let _datas = [];
+yearOne.onSnapshot(function (snapshotData) {
+  snapshotData.forEach(function (doc) {
+    let dataOne = doc.data();
+    dataOne.id = doc.id;
+    _datas.push(dataOne)
+  })
+});
+console.log(_datas)
+
+function appendData(datas) {
+  for (let data in datas) {
+   let _dataPrYear = [{
+      year: "2015",
+      yearCows: data.yearCows
+    }];
+    console.log(_dataPrYear)
+  }
+}
+appendData(_datas)
+
+let dataPrYear = [{
+  year: "2015",
+  cows: 51
+}, 
+{
+  year: "2016",
+  cows: 50
+}, 
+{
+  year: "2017",
+  cows: 49
+}, 
+{
+  year: "2018",
+  cows: 51
+}, 
+{
+  year: "2019",
+  cows: 50
+}
+];
+// 2: prepare data for chart
+// seperating the objects to arrays: months and sales
+// why? that's how chart.js reads the data :)
+function prepareData(data) {
+  // declaring two array to store the data 
+  let years = [];
+  let cows = [];
+  // looping through the data array
+  for (const object of data) {
+    // adding the values to the different arrays
+    years.push(object.year);
+    cows.push(object.cows);
+  }
+  // returning the two arrays (months & sales) inside and object
+  // we cannot return to values - that's why we have to do it inside an array
+  return {
+    years,
+    cows
+  };
+}
+
+// 3: create and append the chart
+function appendChart(cowData) {
+  // using prepareData() to get the excact data we want
+  let data = prepareData(cowData);
+  //open the developer console to inspect the result
+  console.log(data);
+  let chartContainer = document.getElementById('chartContainer');
+  let chart = new Chart(chartContainer, {
+    // The type of chart we want to create
+    type: 'line',
+    // The data for our dataset
+    data: {
+      labels: data.years, // refering to the data object, holding data from prepareData()
+      datasets: [{
+        data: data.cows, // refering to the data object, holding data from prepareData()
+        label: 'Cows each year',
+        backgroundColor: '#f1f1f1', // Customise the graf color etc. Go to the docs to find more: https://www.chartjs.org/docs/latest/
+        borderColor: 'rgb(255, 99, 132)'
+      }]
+    },
+    // Configuration options goes here
+    // Go to the docs to find more: https://www.chartjs.org/docs/latest/
+    options: {
+      title: {
+        display: true,
+        text: 'Cows'
+      }
+    }
+
+  });
+}
+
+appendChart(dataPrYear);
+
+
