5d8477a223c3dac3ef356b6bc3d3cc846f40ac99
Neibye <60384297+Neibye@users.noreply.github.com>
2020-11-08 01:41:40 +0100

    idk

--- js/main.js
+++ js/main.js
@@ -65,4 +65,105 @@ function appendData(datas) {
 }
 appendData(_datas)
 */
+let _firebaseUI;
 
+// ========== FIREBASE AUTH ========== //
+// Listen on authentication state change
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) { // if user exists and is authenticated
+    userAuthenticated(user);
+  } else { // if user is not logged in
+    userNotAuthenticated();
+  }
+});
+
+
+
+function userNotAuthenticated() {
+  showPage("home");
+
+  // Firebase UI configuration
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: '#side1'
+  };
+  // Init Firebase UI Authentication
+  if (!_firebaseUI) {
+    _firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
+  } 
+  _firebaseUI.start('#firebaseui-auth-container', uiConfig);
+  showLoader(false);
+}
+
+
+
+
+
+
+/*
+
+let _firebaseUI;
+
+// ========== FIREBASE AUTH ========== //
+// Listen on authentication state change
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) { // if user exists and is authenticated
+    userAuthenticated(user);
+  } else { // if user is not logged in
+    userNotAuthenticated();
+  }
+});
+
+function userAuthenticated(user) {
+  appendUserData(user);
+}
+
+function userNotAuthenticated() {
+  showPage("login");
+
+  // Firebase UI configuration
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: '#side1'
+  };
+  // Init Firebase UI Authentication
+  if (!_firebaseUI) {
+    _firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
+  } 
+  _firebaseUI.start('#firebaseui-auth-container', uiConfig);
+  
+}
+
+// show and hide tabbar
+function hideTabbar(hide) {
+  let tabbar = document.querySelector('#tabbar');
+  if (hide) {
+    tabbar.classList.add("hide");
+  } else {
+    tabbar.classList.remove("hide");
+  }
+}
+
+
+// sign out user
+function logout() {
+  firebase.auth().signOut();
+}
+
+function appendUserData(user) {
+  document.querySelector('#profile').innerHTML += `
+    <h3>${user.displayName}</h3>
+    <p>${user.email}</p>
+  `;
+}
+*/
